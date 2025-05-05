bf7a297b98e37b2f29b4c09e53d8d34a2dbf2b05
GabrielCorrea12 <gabriel_o_correa@estudante.sesisenai.org.br>
2025-05-05 10:10:53 -0300

    Create atividade2.py

---
+++ atividade2.py
@@ -0,0 +1,6 @@
+salario_minimo = float(input("qual é o salario minimo?"))
+salario_funcionario = float(input("qual é o salario bruto?"))
+
+qtd = salario_minimo / salario_funcionario
+
+print (f"O total{qtd} de salario minimo é:")
